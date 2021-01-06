57ce5815f285a93882ad96816958c73f4d40be4e
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-05 23:09:53 -0300

    Add switch with 2 rails

--- src/railfencecipher/RailFenceCipher.java
+++ src/railfencecipher/RailFenceCipher.java
@@ -8,6 +8,24 @@ public class RailFenceCipher {
         int rail = 2;
         char[] splitPlaintText = plainText.toCharArray();
         char[][] encryptedMatrix = new char[rail][plainText.length()];
+
+        switch (rail) {
+            case 2:
+                for (int line = 0; line < rail; line++) { //For 2 rails
+                    for (int column = 0; column < plainText.length(); column++) {
+                        if (line == column) {
+                            encryptedMatrix[line][column] = splitPlaintText[column];
+                        }
+                        if (line == 0 && column % 2 == 0) {
+                            encryptedMatrix[line][column] = splitPlaintText[column];
+                        }
+                        if (line == 1 && column % 2 != 0) {
+                            encryptedMatrix[line][column] = splitPlaintText[column];
+                        }
+                    }
+                }
+            break;
+        }
     }
-    
+
 }

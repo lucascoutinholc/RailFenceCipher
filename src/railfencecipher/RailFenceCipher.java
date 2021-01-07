4a6b9eae634e3d1df96c23abe7e31aac28c3fc90
Lucas Coutinho <lukascoutinhoalmeida@gmail.com>
2021-01-07 20:12:45 -0300

    Add switch with 3 rails

--- src/railfencecipher/RailFenceCipher.java
+++ src/railfencecipher/RailFenceCipher.java
@@ -24,7 +24,28 @@ public class RailFenceCipher {
                         }
                     }
                 }
-            break;
+                break;
+            case 3:
+                int aux = 0;
+                int aux2 = 1;
+                int aux3 = 2;
+                for (int line = 0; line < rail; line++) {
+                    for (int column = 0; column < plainText.length(); column++) {
+                        if (line == 0 && aux < plainText.length()) {
+                            encryptedMatrix[line][aux] = splitPlaintText[aux];
+                            aux += 4;
+                        }
+                        if (line == 1 && aux2 < plainText.length()) {
+                            encryptedMatrix[line][aux2] = splitPlaintText[aux2];
+                            aux2 += 2;
+                        }
+                        if (line == 2 && aux3 < plainText.length()) {
+                            encryptedMatrix[line][aux3] = splitPlaintText[aux3];
+                            aux3 += 4;
+                        }
+                    }
+                }
+                break;
         }
     }
 
